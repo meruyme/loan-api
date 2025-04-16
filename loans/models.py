e05b22d613e15d1db8fd40e6b8ee23b3bf0c866b
Mellany Linhares <mellinhares6@gmail.com>
2025-04-16 15:56:39 -0300

    feat: add loans app and models

---
+++ loans/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Bank(models.Model):
+    name = models.CharField(
+        max_length=255
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Loan(models.Model):
+    value = models.DecimalField(max_digits=11, decimal_places=2)
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    ip_address = models.CharField(max_length=32)
+    bank = models.ForeignKey(Bank, on_delete=models.PROTECT, related_name='loans')
+    client = models.ForeignKey(User, on_delete=models.PROTECT, related_name='loans')
+    requested_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.id
+
+
+class LoanPayment(models.Model):
+    value = models.DecimalField(max_digits=11, decimal_places=2)
+    loan = models.ForeignKey(Loan, on_delete=models.PROTECT, related_name='payments')
+    paid_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.id
