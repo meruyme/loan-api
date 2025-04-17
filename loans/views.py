f346a7b5e7965d33f6338f151234f761562432b4
Mellany Linhares <mellinhares6@gmail.com>
2025-04-16 21:35:46 -0300

    feat: add views

--- loans/views.py
+++ loans/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from loans.models import Loan, LoanPayment
+from loans.serializers import LoanSerializer, LoanPaymentSerializer
+
+
+class LoanViewSet(viewsets.ModelViewSet):
+    serializer_class = LoanSerializer
+
+    def get_queryset(self):
+        return Loan.objects.filter(client=self.request.user).prefetch_related("payments").order_by('requested_at')
+
+
+class LoanPaymentViewSet(viewsets.ModelViewSet):
+    serializer_class = LoanPaymentSerializer
+
+    def get_queryset(self):
+        return LoanPayment.objects.filter(loan__client=self.request.user).order_by('paid_at')
